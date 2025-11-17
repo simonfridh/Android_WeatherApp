a77fdb2e738944ba92506d823df85c16b0a6e2c1
simonfridh <sfridh@kth.se>
2025-11-17 17:33:56 +0100

    Test homescreen with different modes for landscape and portrait

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,21 +35,9 @@ class MainActivity : ComponentActivity() {
                         startDestination = "HomeScreen"
                     ) {
                         composable("HomeScreen") {
-                            HomeScreen(
-                                navController = navController
-                            )
+                            HomeScreen()
                         }
                     }
-
-
-
-
-
-
-
-
-
-
                 }
             }
         }
--- app/src/main/java/com/example/weather/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/weather/ui/screens/HomeScreen.kt
@@ -1,58 +1,181 @@
 package com.example.weather.ui.screens
 
+import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(
-    navController: NavController
+
 ) {
 
-    //Values
+    val orientation = LocalConfiguration.current.orientation
 
     Scaffold {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
 
-            //TOP SECTION
-            Row() {
-
-                Text("TOP")
-
-            }
-
-            //MID SECTION
+        //PORTRAIT MODE
+        if(orientation == Configuration.ORIENTATION_PORTRAIT) {
             Column(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
-                Text("MID")
+                //FIRST SECTION
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.primary),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text= "First", color = MaterialTheme.colorScheme.onPrimary)
+                    }
+
+                }
+
+                //SECOND SECTION
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.secondary),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text= "Second", color = MaterialTheme.colorScheme.onSecondary)
+                    }
+
+                }
+
+                //THIRD SECTION
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.tertiary),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text= "Third", color = MaterialTheme.colorScheme.onTertiary)
+                    }
+
+                }
             }
+        }
 
-            //BOTTOM SECTION
-            Row() {
 
-                Text("BOTTOM")
+        //LANDSCAPE MODE
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                //LEFT SIDE - FIRST SECTION
+                Column (
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.primary),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text= "First", color = MaterialTheme.colorScheme.onPrimary)
+                    }
+                }
+
+                //RIGHT SIDE
+                Column (
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    //SECOND SECTION
+                    Column (
+                        modifier = Modifier
+                            .weight(1f),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(MaterialTheme.colorScheme.secondary),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text= "Second", color = MaterialTheme.colorScheme.onSecondary)
+                        }
+                    }
+
+                    //THIRD SECTION
+                    Column (
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(MaterialTheme.colorScheme.tertiary),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text= "Third", color = MaterialTheme.colorScheme.onTertiary)
+                        }
+                    }
+                }
 
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreviewPortrait() {
+    HomeScreen()
+}
+
+@Preview(
+    widthDp = 915,
+    heightDp = 412
+)
+@Composable
+fun LandscapePreview() {
+    HomeScreen()
 }
