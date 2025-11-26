15cb5fab77d0490be0484359b371e5ca43c5b297
simonfridh <sfridh@kth.se>
2025-11-26 18:40:40 +0100

    Fixed timezone for fetched data

--- app/src/main/java/com/example/weather/data/remote/mapper/ForecastMapper.kt
+++ app/src/main/java/com/example/weather/data/remote/mapper/ForecastMapper.kt
@@ -5,6 +5,7 @@ import com.example.weather.domain.models.Forecast
 import com.example.weather.domain.models.Weather
 import com.example.weather.domain.models.WeatherIcon
 import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 fun ForecastResponseDto.toForecast(): Forecast {
@@ -13,9 +14,17 @@ fun ForecastResponseDto.toForecast(): Forecast {
         throw Exception("No valid forecast received")
     }
 
+    //calculate how many hours to add to time based on timezone
+    val localZone = ZoneId.systemDefault()
+    val offset = localZone.rules.getOffset(LocalDateTime.now())
+    val timeZoneHourOffset = offset.totalSeconds / 3600L
+
     //parse weather data
     val currentWeather = Weather.Current(
-        time = LocalDateTime.parse(current.time, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")),
+        time = LocalDateTime.parse(
+            current.time,
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")
+        ).plusHours(timeZoneHourOffset),
         weatherIcon = WeatherIcon.iconFromWeatherCode(current.weather_code),
         temperature = current.temperature_2m.toFloat()
     )
@@ -24,7 +33,10 @@ fun ForecastResponseDto.toForecast(): Forecast {
     for(i in 0 until hourly.time.size) {
         hourlyWeather.add(
             Weather.Hourly(
-                time = LocalDateTime.parse(hourly.time[i], DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")),
+                time = LocalDateTime.parse(
+                    hourly.time[i],
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")
+                ).plusHours(timeZoneHourOffset),
                 weatherIcon = WeatherIcon.iconFromWeatherCode(hourly.weather_code[i]),
                 temperature = hourly.temperature_2m[i].toFloat()
             )
@@ -35,7 +47,10 @@ fun ForecastResponseDto.toForecast(): Forecast {
     for(i in 0 until daily.time.size) {
         dailyWeather.add(
             Weather.Daily(
-                time = LocalDateTime.parse(daily.time[i] + "T00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")),
+                time = LocalDateTime.parse(
+                    daily.time[i] + "T00:00",
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm")
+                ),
                 weatherIcon = WeatherIcon.iconFromWeatherCode(daily.weather_code[i]),
                 maxTemperature = daily.temperature_2m_max[i].toFloat(),
                 minTemperature = daily.temperature_2m_min[i].toFloat()
